d9a04e85f1071d16f21300e634f76aa79dea1a59
Vu Cao <cdhvu@yahoo.com>
2018-06-24 11:06:01 -0700

    Added state to app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      list: [
+        {
+          task: "Organize Garage",
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: "Bake Cookie",
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        Hello
+        <h1>
+          Hello
+        </h1>
       </div>
     );
   }
